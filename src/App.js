dbef5a0f4e5601ddf1fe3497e3564f28cbe73867
carloshlopez <carloshlopez@gmail.com>
2021-06-26 16:32:52 +0000

    2nd change with amplilfy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>This is the new me!</h1>
+          <h1>Te amo pelotita!</h1>
         </p>
         <a
           className="App-link"
